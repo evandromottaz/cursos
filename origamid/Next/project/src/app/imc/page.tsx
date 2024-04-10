c32bc3a3231bb54bf9c887944b0bd726f2f34c29
evandromottazgx2 <evandro.zambini@gx2.com.br>
2024-04-10 08:41:42 -0300

    feat: show result component

--- origamid/Next/project/src/app/imc/page.tsx
+++ origamid/Next/project/src/app/imc/page.tsx
@@ -1,1 +1,10 @@
-export default function Imc() {}
+import ShowImc from "@/components/show-imc";
+
+export default function ImcPage() {
+	return (
+		<div>
+			<h1>Imc Page</h1>
+			<ShowImc />
+		</div>
+	);
+}
