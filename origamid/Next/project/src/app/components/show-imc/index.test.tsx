d58ec1c3d9030270c6d693b8a0d1ec8c762954eb
evandromottazgx2 <evandro.zambini@gx2.com.br>
2024-04-18 07:58:57 -0300

    feat: fn get imc category by imc and tests

--- origamid/Next/project/src/app/components/show-imc/index.test.tsx
+++ origamid/Next/project/src/app/components/show-imc/index.test.tsx
@@ -1,4 +1,9 @@
-import { calcImc } from "@/components/show-imc";
+import {
+	IMC_ERROR_MESSAGE,
+	calcImc,
+	getImcCategory,
+	imcCategories,
+} from "@/components/show-imc";
 
 describe("calcImc()", () => {
 	it("should show imc of height 1.8 and weight 50kg is 13.8", () => {
@@ -14,5 +19,37 @@ describe("calcImc()", () => {
 	it("should show imc of height 2 and weight 70kg is 17.5", () => {
 		const imc = calcImc({ height: 2, weight: 70 });
 		expect(imc).toBe(17.5);
+	});
+});
+
+describe("getImcCategory(imc)", () => {
+	it("should show the category THINNESS with IMC below 18", () => {
+		const category = getImcCategory(17);
+		expect(category).toBe(imcCategories.THINNESS);
+	});
+
+	it("should show the category NORMAL with IMC below 18.5", () => {
+		const category = getImcCategory(18.5);
+		expect(category).toBe(imcCategories.NORMAL);
+	});
+
+	it("should show the category OVERWEIGHT with IMC above 25.6", () => {
+		const category = getImcCategory(25.6);
+		expect(category).toBe(imcCategories.OVERWEIGHT);
+	});
+
+	it("should show the category OBESITY with IMC above 30", () => {
+		const category = getImcCategory(30);
+		expect(category).toBe(imcCategories.OBESITY);
+	});
+
+	it("should show the category SEVERE_OBESITY with IMC above 40", () => {
+		const category = getImcCategory(40);
+		expect(category).toBe(imcCategories.SEVERE_OBESITY);
+	});
+
+	it("should show message if no IMC was passed", () => {
+		const error = getImcCategory();
+		expect(error).toBe(IMC_ERROR_MESSAGE);
 	});
 });
--- origamid/Next/project/src/app/components/show-imc/index.tsx
+++ origamid/Next/project/src/app/components/show-imc/index.tsx
@@ -7,13 +7,47 @@ interface CalcImcProps {
 	weight: number;
 }
 
+export const IMC_ERROR_MESSAGE = "Você não passou um IMC para calcular!";
+
+export const imcCategories = {
+	THINNESS: "MAGREZA",
+	NORMAL: "NORMAL",
+	OVERWEIGHT: "SOBREPESO",
+	OBESITY: "OBESIDADE",
+	SEVERE_OBESITY: "OBESIDADE GRAVE",
+};
+
 export function calcImc({ height, weight }: CalcImcProps) {
 	const imc = weight / (height * 2);
 	return +imc.toFixed(2).replace(/\d$/, "");
 }
 
+export function getImcCategory(imc: number) {
+	if (imc < 18.5) {
+		return imcCategories.THINNESS;
+	}
+
+	if (imc >= 18.5 && imc < 25) {
+		return imcCategories.NORMAL;
+	}
+
+	if (imc >= 25 && imc < 30) {
+		return imcCategories.OVERWEIGHT;
+	}
+
+	if (imc >= 30 && imc < 40) {
+		return imcCategories.OBESITY;
+	}
+
+	if (imc >= 40) {
+		return imcCategories.SEVERE_OBESITY;
+	}
+
+	return IMC_ERROR_MESSAGE;
+}
+
 export default function ShowImc() {
-	const [result, setResult] = useState("");
+	const [result, setResult] = useState(0);
 
 	const onSubmit = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
@@ -23,7 +57,7 @@ export default function ShowImc() {
 		const weight = +form.get("weight")!;
 
 		const imc = calcImc({ height, weight });
-		setResult(imc.toString());
+		setResult(imc);
 	};
 
 	return (
@@ -34,7 +68,12 @@ export default function ShowImc() {
 				<button>Calcular</button>
 			</form>
 
-			{!!result && <p data-testid="imc-result">Seu IMC é de {result}</p>}
+			{!!result && (
+				<>
+					<p data-testid="imc-result">Seu IMC é de {result}</p>
+					<p>Sua categoria: {getImcCategory(result)}</p>
+				</>
+			)}
 		</>
 	);
 }
