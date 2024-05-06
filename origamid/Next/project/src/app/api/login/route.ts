4218e970ff7bf2d59009971dbd5d5deba70e9577
evandromottazgx2 <evandro.zambini@gx2.com.br>
2024-05-06 09:14:50 -0300

    feat: add api perfil

--- origamid/Next/project/src/app/api/login/route.ts
+++ origamid/Next/project/src/app/api/login/route.ts
@@ -19,3 +19,18 @@ export async function POST(request: NextRequest) {
 
 	return Response.json({ ok: true });
 }
+
+export async function GET() {
+	const token = cookies().get("token");
+
+	const response = await fetch("https://api.origamid.online/conta/perfil", {
+		method: "GET",
+		headers: {
+			Authorization: "Bearer " + token?.value,
+		},
+	});
+
+	const data = await response.json();
+
+	return Response.json(data);
+}
