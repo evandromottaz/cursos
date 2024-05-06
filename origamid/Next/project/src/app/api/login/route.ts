6b50edd51a72a8fa7678d24bb06f36708bddfd8c
evandromottazgx2 <evandro.zambini@gx2.com.br>
2024-05-06 10:53:26 -0300

    refactor: exercise adjusts

--- origamid/Next/project/src/app/api/login/route.ts
+++ origamid/Next/project/src/app/api/login/route.ts
@@ -14,26 +14,14 @@ export async function POST(request: NextRequest) {
 
 	if (!response.ok) {
 		cookies().delete("token");
-		return Response.json({ error: "Usuário ou senha incorreto" });
+		return Response.json(
+			{ autorizado: false, error: "Usuário ou senha incorreto" },
+			{ status: 401 }
+		);
 	}
 
 	const data = await response.json();
 	cookies().set("token", data.token, { secure: true, httpOnly: true });
-
-	return Response.json({ ok: true });
-}
-
-export async function GET() {
-	const token = cookies().get("token");
-
-	const response = await fetch("https://api.origamid.online/conta/perfil", {
-		method: "GET",
-		headers: {
-			Authorization: "Bearer " + token?.value,
-		},
-	});
-
-	const data = await response.json();
 
 	return Response.json(data);
 }
--- origamid/Next/project/src/components/menu.tsx
+++ origamid/Next/project/src/components/menu.tsx
@@ -1,13 +1,33 @@
-"use client";
+import { cookies } from "next/headers";
 import Link from "next/link";
 
-export default function Menu() {
+type Account = {
+	autorizado: boolean;
+	usuario: string;
+};
+
+export default async function Menu() {
+	const token = cookies().get("token");
+
+	const response = await fetch("https://api.origamid.online/conta/perfil", {
+		method: "GET",
+		headers: {
+			Authorization: "Bearer " + token?.value,
+		},
+	});
+
+	const account: Account = response.ok
+		? ((await response.json()) as Account)
+		: { autorizado: false, usuario: "" };
+
 	return (
 		<ul className="menu">
 			<Link href="/imc">Imc</Link>
 			<Link href="/cursos">Cursos</Link>
 
-			<div>{localStorage.getItem("user") || <Link href="/login">Login</Link>}</div>
+			<div>
+				{account.autorizado ? account.usuario : <Link href="/login">Login</Link>}
+			</div>
 		</ul>
 	);
 }
