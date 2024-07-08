6c3056bfc5ed4ce8b9fbd699b070d467a5bf42a0
evandromottaz <evandro.zambini@gx2.com.br>
2024-07-08 12:08:14 -0300

    feat: class fileWriter and bufferedWriter

---
+++ devSuperior - JAVA/fileWriter_bufferedWriter/src/application/Main.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] lines = new String[]{"Good morning", "Good afternoon", "Good evening"};
+        String path = "c:\\temp\\out.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+    }
+}
