6de5099567624aa8afc653ee9dfcaf7ba3dd83c9
evandromottaz <evandro.motta@live.com>
2024-08-15 10:26:44 -0300

    create seller dao impl

---
+++ devSuperior - JAVA/277-dao_pattern/src/model/dao/impl/SellerDaoImplJDBC.java
@@ -0,0 +1,34 @@
+package model.dao.impl;
+
+import entities.Seller;
+import model.GenericDao;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SellerDaoImplJDBC implements GenericDao<Seller> {
+    @Override
+    public void insert(Seller e) {
+
+    }
+
+    @Override
+    public void update(Seller e) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return Collections.emptyList();
+    }
+}
