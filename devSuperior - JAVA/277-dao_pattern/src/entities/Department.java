64850e55dfabb72bdfc0c1b7947ec145a927e8a3
evandromottaz <evandro.motta@live.com>
2024-08-15 09:54:24 -0300

    create department entity

---
+++ devSuperior - JAVA/277-dao_pattern/src/entities/Department.java
@@ -0,0 +1,56 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+    private static final long serialVersionUID = -473932597411944620L;
+    
+    private Integer id;
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ devSuperior - JAVA/277-dao_pattern/src/program/Program.java
@@ -0,0 +1,7 @@
+package program;
+
+import entities.Department;
+
+public class Program {
+    Department department = new Department(1, "Books");
+}
