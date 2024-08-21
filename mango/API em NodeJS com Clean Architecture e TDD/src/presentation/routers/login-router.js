9546cc92d1e42ea5ff26bb7c52b131c92782200d
evandromottaz <evandro.motta@live.com>
2024-08-21 17:16:05 -0300

    refactor login-router to a class instead factory function

--- mango/API em NodeJS com Clean Architecture e TDD/src/presentation/routers/login-router.js
+++ mango/API em NodeJS com Clean Architecture e TDD/src/presentation/routers/login-router.js
@@ -1,17 +1,18 @@
 const { httpResponse } = require('../helpers/http-response')
 
-function createLoginRouter() {
-    const route = (httpRequest) => {
+module.exports = class LoginRouter {
+    constructor(authUseCase) {
+        this.authUseCase = authUseCase
+    }
+    route(httpRequest) {
         if (!httpRequest || !httpRequest.body) return httpResponse.serverError()
 
         if (!httpRequest.body.email) return httpResponse.badRequest('email')
 
         if (!httpRequest.body.password) return httpResponse.badRequest('password')
 
+        this.authUseCase.auth(httpRequest.body.email)
+
         return httpResponse.badRequest()
     }
-
-    return { route }
 }
-
-module.exports = { createLoginRouter }
