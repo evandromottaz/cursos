8d029294168f7cbaee6ada35f34b551d79d84e75
evandromottaz <evandro.motta@live.com>
2024-08-21 12:39:49 -0300

    add jest

---
+++ mango/API em NodeJS com Clean Architecture e TDD/jest.config.js
@@ -0,0 +1,9 @@
+/**
+/** @type {import('jest').Config} */
+const config = {
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+}
+
+module.exports = config
