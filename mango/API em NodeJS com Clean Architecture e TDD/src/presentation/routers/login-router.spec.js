17a9862ad5ceae41b1831bd0893e28ea23eda30c
evandromottaz <evandro.motta@live.com>
2024-08-21 14:14:55 -0300

    test: createLoginRouter returns 400 if no password is provided

--- mango/API em NodeJS com Clean Architecture e TDD/src/presentation/routers/login-router.spec.js
+++ mango/API em NodeJS com Clean Architecture e TDD/src/presentation/routers/login-router.spec.js
@@ -6,11 +6,21 @@ function createLoginRouter() {
     return { route }
 }
 describe('first', () => {
-    test('should return 400 if not email is proved', () => {
+    test('should return 400 if no email is proved', () => {
         const sut = createLoginRouter()
         const httpRequest = {
             body: {
                 password: 'any_password',
+            },
+        }
+        const httpResponse = sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+    test('should return 400 if no password is proved', () => {
+        const sut = createLoginRouter()
+        const httpRequest = {
+            body: {
+                email: 'any_email@gmail.com',
             },
         }
         const httpResponse = sut.route(httpRequest)
