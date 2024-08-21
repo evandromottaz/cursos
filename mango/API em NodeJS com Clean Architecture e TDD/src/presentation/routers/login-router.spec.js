1277bf6aea5d092dca5770b22922c8e790b4c4bb
evandromottaz <evandro.motta@live.com>
2024-08-21 14:22:19 -0300

    refactor: login-router test order

--- mango/API em NodeJS com Clean Architecture e TDD/src/presentation/routers/login-router.spec.js
+++ mango/API em NodeJS com Clean Architecture e TDD/src/presentation/routers/login-router.spec.js
@@ -12,6 +12,16 @@ function createLoginRouter() {
     return { route }
 }
 describe('first', () => {
+    test('Should return 500 if no httpRequest is proved', () => {
+        const sut = createLoginRouter()
+        const httpResponse = sut.route()
+        expect(httpResponse.statusCode).toBe(500)
+    })
+    test('Should return 500 if httpRequest is invalid', () => {
+        const sut = createLoginRouter()
+        const httpResponse = sut.route({})
+        expect(httpResponse.statusCode).toBe(500)
+    })
     test('Should return 400 if no email is proved', () => {
         const sut = createLoginRouter()
         const httpRequest = {
@@ -31,15 +41,5 @@ describe('first', () => {
         }
         const httpResponse = sut.route(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
-    })
-    test('Should return 500 if no httpRequest is proved', () => {
-        const sut = createLoginRouter()
-        const httpResponse = sut.route()
-        expect(httpResponse.statusCode).toBe(500)
-    })
-    test('Should return 500 if httpRequest is invalid', () => {
-        const sut = createLoginRouter()
-        const httpResponse = sut.route({})
-        expect(httpResponse.statusCode).toBe(500)
     })
 })
