e694b4e8c276ef179bf16ba44ea571dbf8ddb746
evandromottaz <evandro.motta@live.com>
2024-08-22 16:13:54 -0300

    fix false positive test AuthUseCase when auto params are not provided

--- mango/API em NodeJS com Clean Architecture e TDD/src/domain/usecases/auth-usecase.spec.js
+++ mango/API em NodeJS com Clean Architecture e TDD/src/domain/usecases/auth-usecase.spec.js
@@ -23,11 +23,11 @@ describe('AuthUseCase', () => {
     test('Should throw if no email is provided', async () => {
         const sut = new AuthUseCase()
         const promise = sut.auth()
-        expect(promise).rejects.toThrow(new MissingParamError('email'))
+        await expect(promise).rejects.toThrow(new MissingParamError('email'))
     })
     test('Should throw if no password is provided', async () => {
         const sut = new AuthUseCase()
         const promise = sut.auth('any_email')
-        expect(promise).rejects.toThrow(new MissingParamError('password'))
+        await expect(promise).rejects.toThrow(new MissingParamError('password'))
     })
 })
