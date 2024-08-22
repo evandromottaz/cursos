c7b700eb90286dc434fdd5ea0dd6dfaa9ced4b08
evandromottaz <evandro.motta@live.com>
2024-08-22 16:02:57 -0300

    ensure what kind of error AuthUseCase throw

--- mango/API em NodeJS com Clean Architecture e TDD/src/domain/usecases/auth-usecase.spec.js
+++ mango/API em NodeJS com Clean Architecture e TDD/src/domain/usecases/auth-usecase.spec.js
@@ -20,6 +20,6 @@ describe('AuthUseCase', () => {
     test('Should throw if no email is provided', async () => {
         const sut = new AuthUseCase()
         const promise = sut.auth()
-        expect(promise).rejects.toThrow()
+        expect(promise).rejects.toThrow(new MissingParamError('email'))
     })
 })
