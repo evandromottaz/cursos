718b3328892de4af1245f38d1e758ebc5580e396
evandromottaz <evandro.motta@live.com>
2024-08-22 14:39:04 -0300

    ensure EmailValidator returns the same email of validator

--- mango/API em NodeJS com Clean Architecture e TDD/src/utils/email-validator.spec.js
+++ mango/API em NodeJS com Clean Architecture e TDD/src/utils/email-validator.spec.js
@@ -1,12 +1,31 @@
+const validator = {
+    isEmailValid: true,
+    isEmail(email) {
+        this.email = email
+        return this.isEmailValid
+    },
+}
+
 class EmailValidator {
     isValid(email) {
-        return true
+        return validator.isEmail(email)
     }
 }
 
 describe('Email Validator', () => {
     test('Should return true if validator returns true', () => {
         const sut = new EmailValidator()
-        expect(sut.isValid('4vz2n@example.com')).toBe(true)
+        expect(sut.isValid('valid_email@gmail.com')).toBe(true)
+    })
+
+    test('Should return false if validator returns false', () => {
+        validator.isEmailValid = false
+        const sut = new EmailValidator()
+        expect(sut.isValid('invalid_email@gmail.com')).toBe(false)
+    })
+    test('Should call validator with correct email', () => {
+        const sut = new EmailValidator()
+        sut.isValid('any_email@gmail.com')
+        expect(validator.email).toBe('any_email@gmail.com')
     })
 })
