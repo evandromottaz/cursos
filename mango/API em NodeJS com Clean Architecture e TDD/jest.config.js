1a0537f0cd552afbaf41237af957d12c01ba4c39
evandromottaz <evandro.motta@live.com>
2024-08-22 16:55:05 -0300

    add jest configs

--- mango/API em NodeJS com Clean Architecture e TDD/jest.config.js
+++ mango/API em NodeJS com Clean Architecture e TDD/jest.config.js
@@ -4,4 +4,6 @@ export default {
     collectCoverage: false,
     transformIgnorePatterns: ['/node_modules/', '/eslint.config.mjs/'],
     testEnvironment: 'node',
+    noStackTrace: true,
+    silent: true,
 }
